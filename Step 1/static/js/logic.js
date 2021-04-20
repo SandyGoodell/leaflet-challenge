e19337bd1816dbc92dcad49d2fbdc18609c3f50a
Sandy Goodell <sgoodel3@uoregon.edu>
2021-04-19 20:17:50 -0700

    map layer

--- Step 1/static/js/logic.js
+++ Step 1/static/js/logic.js
@@ -16,4 +16,18 @@
 /    }).catch(e => console.warn(e));
 /
 / ----------------------------------------*/
+// Create the map object with options.
+var myMap = L.map("map", {
+    center: [0,0],
+    zoom: 2
+});
 
+// Add a tile layer - this will be the background of the map object
+L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/dark-v10",
+  accessToken: API_KEY
+}).addTo(myMap);
